097a11dd4fb4444e46d31bcf237bd6549167d78f
stephendj <stephendj181194@gmail.com>
2015-11-13 16:34:53 +0700

    Added Sigmoid Function

---
+++ src/myann/SigmoidFunction.java
@@ -0,0 +1,10 @@
+package myann;
+
+public class SigmoidFunction extends ActivationFunction {
+
+    @Override
+    public double calculateOutput(double net) {
+        return (1/(1+Math.exp(-net)));
+    }
+    
+}
