e8642dfe918a865bcace9fe2100a13ac9ca66cad
stephendj <stephendj181194@gmail.com>
2015-11-13 20:54:54 +0700

    Modify Main

--- src/myann/MyANN.java
+++ src/myann/MyANN.java
@@ -1,9 +1,15 @@
 package myann;
 
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
 public class MyANN {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    private static final String DATASET1 = "data/dataset2.arff";
     
+    public static void main(String[] args) {
+        Instances instances = Helper.loadDataFromFile(DATASET1);
+        Classifier classifierPTR = Helper.buildClassifier(instances, "ptr");
+    }
+
 }
