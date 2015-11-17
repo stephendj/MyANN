918a736a2e69167bf2050aa8f5c24ebb1d13a828
susantigojali <susantigojali@hotmail.com>
2015-11-17 19:28:52 +0700

    Add javadoc for Single Layer Perceptron

--- src/myann/SingleLayerPerceptron.java
+++ src/myann/SingleLayerPerceptron.java
@@ -14,6 +14,13 @@ public abstract class SingleLayerPerceptron extends Classifier {
     private double m_LearningRate;
     private double m_Momentum;
 
+    /**
+     *
+     * @param m_MaxIteration maximum iteration
+     * @param m_Neuron neuron
+     * @param m_LearningRate learning rate
+     * @param m_Momentum momentum
+     */
     public SingleLayerPerceptron(int m_MaxIteration, Neuron m_Neuron,
             double m_LearningRate, double m_Momentum) {
         this.m_MaxIteration = m_MaxIteration;
@@ -22,6 +29,30 @@ public abstract class SingleLayerPerceptron extends Classifier {
         this.m_Momentum = m_Momentum;
     }
 
+    /**
+     *
+     * @param instance instance
+     * @return output from the instance
+     */
+    public double calculateOutput(Instance instance) {
+        return m_Neuron.calculateOutput(instance);
+    }
+    
+    /**
+     *
+     * @param idx the index of the instance
+     * @return delta bias weight
+     */
+    public double calculateDeltaBiasWeight(int idx) {
+        double error = m_Instances.instance(idx).classValue() - m_Neuron.calculateOutput(m_Instances.instance(idx));
+        return m_LearningRate * m_Neuron.getBias() * error;
+    }
+    
+    /**
+     *
+     * @param idx the index of the instance
+     * @return list of delta weight
+     */
     public List<Double> calculateDeltaWeight(int idx) {
         List<Double> deltaWeights = new ArrayList<>();
         double error = m_Instances.instance(idx).classValue() - m_Neuron.calculateOutput(m_Instances.instance(idx));
@@ -33,11 +64,23 @@ public abstract class SingleLayerPerceptron extends Classifier {
         return deltaWeights;
     }
 
-    public double calculateDeltaBiasWeight(int idx) {
-        double error = m_Instances.instance(idx).classValue() - m_Neuron.calculateOutput(m_Instances.instance(idx));
-        return m_LearningRate * m_Neuron.getBias() * error;
+    /**
+     *
+     * @param deltaWeights list of the delta weight
+     * @param deltaBiasWeight delta of bias weight
+     */
+    public void updateWeights(List<Double> deltaWeights, double deltaBiasWeight) {
+        List<Double> newWeight = new ArrayList<>();
+        for (int i = 0; i < m_Neuron.getWeight().size(); ++i) {
+            newWeight.add(m_Neuron.getWeight().get(i) + deltaWeights.get(i));
+        }
+        m_Neuron.setWeight(newWeight);
+        m_Neuron.setBiasWeight(m_Neuron.getBiasWeight() + deltaBiasWeight);
     }
-
+    /**
+     *
+     * @return mean square error for 1 epoch
+     */
     public double calculateMSE() {
         double sum = 0;
         for (int i = 0; i < m_Instances.numInstances(); ++i) {
@@ -47,19 +90,10 @@ public abstract class SingleLayerPerceptron extends Classifier {
         return 0.5 * sum;
     }
 
-    public double calculateOutput(Instance instance) {
-        return m_Neuron.calculateOutput(instance);
-    }
 
-    public void updateWeights(List<Double> deltaWeights, double deltaBiasWeight) {
-        List<Double> newWeight = new ArrayList<>();
-        for (int i = 0; i < m_Neuron.getWeight().size(); ++i) {
-            newWeight.add(m_Neuron.getWeight().get(i) + deltaWeights.get(i));
-        }
-        m_Neuron.setWeight(newWeight);
-        m_Neuron.setBiasWeight(m_Neuron.getBiasWeight() + deltaBiasWeight);
-    }
-
+    /**
+     * print the model of neuron
+     */
     public void printModel() {
         System.out.println("w_bias : " + m_Neuron.getBiasWeight());
         for (int i = 0; i < m_Neuron.getWeight().size(); ++i) {
@@ -67,42 +101,82 @@ public abstract class SingleLayerPerceptron extends Classifier {
         }
     }
 
+    /**
+     *
+     * @return maximum iteration
+     */
     public int getMaxIteration() {
         return m_MaxIteration;
     }
 
+    /**
+     *
+     * @param m_MaxIteration maximum iteration
+     */
     public void setMaxIteration(int m_MaxIteration) {
         this.m_MaxIteration = m_MaxIteration;
     }
 
+    /**
+     *
+     * @return instances
+     */
     public Instances getInstances() {
         return m_Instances;
     }
 
+    /**
+     *
+     * @param m_Instances data train
+     */
     public void setInstances(Instances m_Instances) {
         this.m_Instances = m_Instances;
     }
 
+    /**
+     *
+     * @return neuron
+     */
     public Neuron getNeuron() {
         return m_Neuron;
     }
 
+    /**
+     *
+     * @param m_Neuron neuron
+     */
     public void setNeuron(Neuron m_Neuron) {
         this.m_Neuron = m_Neuron;
     }
 
+    /**
+     *
+     * @return learning rate
+     */
     public double getLearningRate() {
         return m_LearningRate;
     }
 
+    /**
+     *
+     * @param m_LearningRate learning rate
+     */
     public void setLearningRate(double m_LearningRate) {
         this.m_LearningRate = m_LearningRate;
     }
 
+    /**
+     *
+     * @return momentum
+     */
     public double getMomentum() {
         return m_Momentum;
     }
 
+    /**
+     *
+     * @param m_Momentum momentum
+     */
     public void setMomentum(double m_Momentum) {
         this.m_Momentum = m_Momentum;
     }
