7034e39767df6a23e02e9be8bb836dadf5e16d9a
susantigojali <susantigojali@hotmail.com>
2015-11-17 19:27:43 +0700

    Add javadoc for Activation Function

--- src/myann/ActivationFunction.java
+++ src/myann/ActivationFunction.java
@@ -2,5 +2,10 @@ package myann;
 
 public abstract class ActivationFunction {
 
+    /**
+     *
+     * @param net value to be calculated
+     * @return calculate the value based on the activation function
+     */
     public abstract double calculateOutput(double net);
 }
