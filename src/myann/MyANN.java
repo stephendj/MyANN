f3ad753926e79da1e8064c6da80674feca8cb273
stephendj <stephendj181194@gmail.com>
2015-11-25 08:44:41 +0700

    Added Nominal to Binary converter

--- src/myann/MyANN.java
+++ src/myann/MyANN.java
@@ -6,12 +6,12 @@ import weka.core.Instances;
 
 public class MyANN {
 
-    private static final String DATASET1 = "data/weather.nominal.arff";
+    private static final String DATASET1 = "data/activity.arff";
     
     public static void main(String[] args) {
         Instances instances = Helper.loadDataFromFile(DATASET1);
         //Classifier classifierPTR = Helper.buildClassifier(instances, "ptr");
-        System.out.println(NominalConverter.nominalToNumeric(instances));
+        System.out.println(NominalConverter.nominalToBinary(instances, false));
     }
 
 }
--- src/myann/nominalconverter/NominalConverter.java
+++ src/myann/nominalconverter/NominalConverter.java
@@ -4,8 +4,16 @@ import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NominalToBinary;
 
 public class NominalConverter {
+
+    /**
+     *
+     * @param instances instances to be converted
+     * @return new instances whose attributes have been converted to numeric
+     */
     public static Instances nominalToNumeric(Instances instances) {
         FastVector newAttributes = new FastVector(instances.numAttributes());
         for(int i = 0; i < instances.numAttributes(); ++i) {
@@ -28,5 +36,29 @@ public class NominalConverter {
         }
         
         return newInstances;
+    }
+    
+    /**
+     *
+     * @param instances instances to be converted
+     * @param compact true if not all attributes will be converted, otherwise false
+     * @return new instances whose attributes have been converted
+     */
+    public static Instances nominalToBinary(Instances instances, boolean compact) {
+        NominalToBinary nominalToBinaryFilter = new NominalToBinary();
+        try {
+            nominalToBinaryFilter.setInputFormat(instances);
+            String[] options;
+            if (compact) {
+                options= new String[]{"A"};
+            } else {
+                options= new String[]{"-A"};
+            }
+            nominalToBinaryFilter.setOptions(options);
+            return Filter.useFilter(instances, nominalToBinaryFilter);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return instances; //Unable to convert
     }
 }
