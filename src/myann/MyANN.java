99650533af9652a0aad8bd7c1cc53d6765d778b6
susantigojali <susantigojali@hotmail.com>
2015-11-29 20:05:54 +0700

    Delete some code

--- src/myann/MyANN.java
+++ src/myann/MyANN.java
@@ -6,19 +6,15 @@ import weka.core.Instances;
 
 public class MyANN {
 
-    private static final String DATASET = "data/iris.arff";
-    private static final String DATASET_UNLABELED = "data/iris.unlabeled.arff";
+    private static final String DATASET = "data/weater.nominal.arff";
+    private static final String DATASET_UNLABELED = "data/weather.nominal.unlabeled.arff";
     
     public static void main(String[] args) throws Exception {
         Instances instances = Helper.loadDataFromFile(DATASET);
         Instances unlabeledInstances = Helper.loadDataFromFile(DATASET_UNLABELED);
-        System.out.println(NominalConverter.nominalToNumeric(instances));
-        System.out.println(NominalConverter.nominalToNumeric(unlabeledInstances));
-        unlabeledInstances = NominalConverter.nominalToNumeric(unlabeledInstances);
+        
         Classifier classifier = Helper.buildClassifier(instances, "mlp");
-        for (int i=0; i<unlabeledInstances.numInstances(); ++i) {
-            System.out.println(classifier.classifyInstance(unlabeledInstances.instance(i)));
-        }
+        Helper.classifyUsingModel(classifier, DATASET_UNLABELED);
     }
 
 }
